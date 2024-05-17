99c4a6bcf47958d8c59a849b537f0ab5ac44cd19
xingluo233 <xingluo233@outlook.com>
2024-05-18 00:56:38 +0800

    新增不可能的世界每日签到

--- src/scripts/bkneng.js
+++ src/scripts/bkneng.js
@@ -8,16 +8,17 @@ MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQClTsX4wJRr7xW7EKVUtSUg/K1frM2UGUOUya6MQCXw
 let aesKey = Buffer.from(config.bkneng.aesKey, "base64");
 let aesData = Buffer.from(config.bkneng.aesData, "base64");
 
-let n4 = config.bkneng.n4
-let n5 = config.bkneng.n5
-let device = config.bkneng.device
-let token = config.bkneng.token
-let username = config.bkneng.username
+let n4 = config.bkneng.n4;
+let n5 = config.bkneng.n5;
+let n7 = config.bkneng.n7;
+let device = config.bkneng.device;
+let token = config.bkneng.token;
+let username = config.bkneng.username;
 
 let key = Buffer.from(crypto.publicDecrypt({
     key: publicKey,
     padding: crypto.constants.RSA_PKCS1_PADDING
-}, aesKey).toString(), "base64")
+}, aesKey).toString(), "base64");
 
 
 let decipher = crypto.createDecipheriv('aes-128-cbc', key, key);
@@ -28,17 +29,26 @@ ${decrypted.userPriKey}
 
 async function post(options) {
     let time = new Date().getTime();
-    let sign = crypto.createSign('SHA1').update(`${new URLSearchParams(options.data)}&device=__${device}&timestamp=${time}&token=${token}&username=${username}`).sign(userPriKey, 'base64');
+    if(options.data.transId === "") {
+        options.data.transId = device + time
+    }
+    let data = new URLSearchParams(options.data);
+    let param = new URLSearchParams(`device=${device}&timestamp=${time}&token=${token}&username=${username}`);
+    for (let [key, val] of param.entries()) {
+        data.append(key, val);
+    }
+    let sign = crypto.createSign('SHA1').update(data.toString()).sign(userPriKey, 'base64');
     return await got({
         url: `https://gt.bkneng.com/${options.url}`,
         method: "post",
         form: {
-            n1: 100018,
+            n1: 100015,
             n2: 10064,
             n3: 2,
             n4: n4,
             n5: n5,
             n6: "com.bkneng.reader",
+            n7: n7,
             ...options.data,
             device: device,
             sign: sign,
@@ -57,20 +67,26 @@ async function post(options) {
 
 async function get(options) {
     let time = new Date().getTime();
-    let sign = crypto.createSign('SHA1').update(`${new URLSearchParams(options.data)}&device=__${device}&timestamp=${time}&token=${token}&username=${username}`).sign(userPriKey, 'base64');
+    let data = new URLSearchParams(options.data);
+    let param = new URLSearchParams(`device=${device}&timestamp=${time}&token=${token}&username=${username}`);
+    for (let [key, val] of param.entries()) {
+        data.append(key, val);
+    }
+    let sign = crypto.createSign('SHA1').update(data.toString()).sign(userPriKey, 'base64');
     return await got({
         url: `https://gt.bkneng.com/${options.url}`,
         method: "get",
         searchParams: {
-            n1: 100018,
+            n1: 100015,
             n2: 10064,
             n3: 2,
             n4: n4,
             n5: n5,
             n6: "com.bkneng.reader",
+            n7: n7,
             ...options.data,
             device: device,
-            sign: encodeURIComponent(sign),
+            sign: sign,
             timestamp: time,
             token: token,
             username: username
@@ -98,8 +114,43 @@ async function sign() {
     })
 }
 
+//看广告领代券
+async function ad() {
+    return await post({
+        url: "user/out/ad/grantReward",
+        data: {
+            origin: 2,
+            transId: ""
+        }
+    }).then((res) => {
+        return res;
+    })
+}
+
+//看广告剩余次数
+async function getad() {
+    return await get({
+        url: "user/out/welfare/allTaskData",
+        data: {}
+    }).then((res) => {
+        return res;
+    })
+}
+
 export default async function main() {
     let result = "【不可能的世界】：";
-    console.log(await sign())
+    let a = await sign();
+    if(a.code === 0) {
+        result += "\n    每日签到：已完成    ";
+    } else {
+        result += `\n    每日签到：${a.msg}    `;
+    }
+    let adcs = await getad();
+    for (let i = 1; i <= adcs.data[0].total - adcs.data[0].own; i++) {
+        await ad();
+    }
+    adcs = await getad();
+    result += `\n    看视频领10阅读券：剩余${adcs.data[0].total - adcs.data[0].own}次机会    `;
+    console.log(result);
     return result;
 }
